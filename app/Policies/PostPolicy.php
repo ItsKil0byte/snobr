4d3d1ae943fbec28a23f1b99ed182039ddcff24a
Egor Hotenov <teremok1123@mail.ru>
2026-03-20 23:02:36 +0500

    Добавил Middleware и Policy для постов

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Policies;
+
+use App\Enums\Role;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class PostPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(?User $user, Post $post): bool
+    {
+        if($post->published){
+            return true;
+        }
+
+        if(!$user){
+            return false;
+        }
+
+        return $user->id === $post->user_id
+            || $user->role === Role::ADMIN
+            || $user->role === Role::MODERATOR;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user !== null;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Post $post): bool
+    {
+        if(!$user){
+            return false;
+        }
+
+        if($user->role === Role::ADMIN || $user->role === Role::MODERATOR){
+            return true;
+        }
+
+        return $user->id === $post->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Post $post): bool
+    {
+        if(!$user){
+            return false;
+        }
+
+        if($user->role === Role::ADMIN || $user->role === Role::MODERATOR){
+            return true;
+        }
+
+        return $user->id === $post->user_id;
+    }
+}

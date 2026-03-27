a7b10b900fbbbcb57e4453a8c6e40d580f4e327a
Alexander Lebedev <lebalexevg@gmail.com>
2026-03-27 17:01:35 +0500

    add settings service for all views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Services\SettingsService;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('*', function ($view) {
+            $view->with(['settings' => app(SettingsService::class)]);
+        });
     }
 }

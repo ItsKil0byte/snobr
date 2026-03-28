d35ebb44512845dee2706279d3138f2476358deb
Egor Hotenov <teremok1123@mail.ru>
2026-03-28 21:11:25 +0500

    Добавил обработку http ошибок

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Middleware\RoleMiddleware;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,5 +20,24 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->renderable(function(AuthenticationException $e, $request){
+            return response()->json([
+                'error' => 'Unauthorized',
+                'message' => 'You need to log in to continue',
+            ], 401)->header('X-Error-Type', 'auth');
+        });
+
+        $exceptions->renderable(function(AccessDeniedHttpException $e, $request){
+            return response()->json([
+                'error' => 'Forbidden',
+                'message' => 'You do not have permission to do that',
+            ], 403)->header('X-Error-Type', 'auth');
+        });
+
+        $exceptions->renderable(function(NotFoundHttpException $e, $request){
+            return response()->json([
+                'error' => 'Not Found',
+                'message' => 'This resource does not exist',
+            ], 404)->header('X-Error-Type', 'auth');
+        });
     })->create();
